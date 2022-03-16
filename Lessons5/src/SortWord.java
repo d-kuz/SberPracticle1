0c3ddc86d3e42fd40cd9c950b0ddd6cb89215274
d-kuz <d_kuzyakova@mail.ru>
2022-03-16 19:43:56 +0300

    Выполнено 5 задание

--- Lessons5/src/SortWord.java
+++ Lessons5/src/SortWord.java
@@ -3,12 +3,12 @@ import java.util.Arrays;
 
 public class SortWord {
     public static void main(String[] args) throws IOException {
-        File f = new File("C:\\Users\\Natik\\IdeaProjects\\SberPracticle1\\Lessons5\\src\\CopyListNames.txt");
+        File f = new File("C:\\Users\\Natik\\IdeaProjects\\SberPracticle1\\Lessons5\\src\\CopylistNames.txt");
         f.createNewFile();
         FileWriter fileOutputStream = new FileWriter(f);
         String[] a;
         try (BufferedReader fileInputStream = new BufferedReader(new FileReader
-                ("C:\\Users\\Natik\\IdeaProjects\\SberPracticle1\\Lessons5\\src\\ListNames.txt"))){
+                ("C:\\Users\\Natik\\IdeaProjects\\SberPracticle1\\Lessons5\\src\\listNames.txt"))){
             String b = fileInputStream.readLine();
             a = b.split(", ");
             Arrays.sort(a);
